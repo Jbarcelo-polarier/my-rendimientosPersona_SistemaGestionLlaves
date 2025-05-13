82a927a739bb4e151185a3b055b5ad426f08b913
Jbarcelo-polarier <jbarcelo.polarier@outlook.com>
2025-05-13 15:43:29 +0200

    pagina principal creada

---
+++ src/pages/components/PaginaTaquillas.js
@@ -0,0 +1,132 @@
+import React from "react";
+import { StyleSheet, View, TouchableOpacity, Text } from "react-native";
+import { colors } from "../../../styles/base";
+import { useSafeAreaInsets } from "react-native-safe-area-context";
+import { MaterialCommunityIcons } from "@expo/vector-icons";
+import { useNavigation } from "@react-navigation/native";
+
+export default PaginaTaquillas = ({ route }) => {
+  const insets = useSafeAreaInsets();
+  const navigation = useNavigation();
+  const persona = route.params.persona;
+  const vehiculo = route.params.vehiculo;
+  console.log(route.params);
+
+  return (
+    <View style={{ paddingTop: insets.top + 1 }}>
+      <View style={styles.navigationBar}>
+        <TouchableOpacity
+          style={styles.felchaCabecera}
+          onPress={() => navigation.navigate("index")}
+        >
+          <MaterialCommunityIcons
+            name="arrow-left"
+            size={30}
+            color={colors.smokedWhite}
+          />
+        </TouchableOpacity>
+        <Text style={styles.headerText}>Taquillas</Text>
+      </View>
+      <View style={styles.tajetaInfo}>
+        <View style={styles.cabeceraTarjeta}>
+          <Text style={styles.tituloTarjeta}>Informacion Persona</Text>
+        </View>
+        <View style={styles.cuerpoTarjeta}>
+          <Text style={styles.textoTarjeta}>Nombre: {persona.nombre}</Text>
+          <Text style={styles.textoTarjeta}>Apellido: {persona.apellido}</Text>
+        </View>
+      </View>
+      <View style={styles.tajetaInfo}>
+        <View style={styles.cabeceraTarjeta}>
+          <Text style={styles.tituloTarjeta}>{vehiculo.denominacion}</Text>
+        </View>
+        <View style={styles.cuerpoTarjeta}>
+          <Text style={styles.textoTarjeta}>
+            Vehiculo: {vehiculo.tipoVehiculo}
+          </Text>
+          <Text style={styles.textoTarjeta}>
+            Matricula: {vehiculo.matricula}
+          </Text>
+        </View>
+      </View>
+      <View style={styles.contenedorOperacion}>
+        <TouchableOpacity style={styles.botonOperacion}>
+          <Text style={styles.textoOperacion}>Dejar Llave</Text>
+        </TouchableOpacity>
+        <TouchableOpacity style={styles.botonOperacion}>
+          <Text style={styles.textoOperacion}>Recoger Llave</Text>
+        </TouchableOpacity>
+      </View>
+    </View>
+  );
+};
+
+const styles = StyleSheet.create({
+  navigationBar: {
+    flexDirection: "row",
+    width: "100%",
+    height: 60,
+    backgroundColor: colors.primary,
+    justifyContent: "center",
+    alignItems: "center",
+  },
+  felchaCabecera: {
+    position: "absolute",
+    left: 30,
+  },
+  headerText: {
+    color: colors.white,
+    fontSize: 18,
+  },
+  tajetaInfo: {
+    backgroundColor: colors.white,
+    height: 150,
+    width: "80%",
+    alignSelf: "center",
+    marginTop: "5%",
+    borderRadius: 10,
+    elevation: 2,
+  },
+  cabeceraTarjeta: {
+    backgroundColor: colors.primary,
+    width: "100%",
+    height: "20%",
+    borderTopRightRadius: 10,
+    borderTopLeftRadius: 10,
+    alignItems: "center",
+    justifyContent: "center",
+  },
+  tituloTarjeta: {
+    color: colors.white,
+    fontSize: 15,
+    fontWeight: "bold",
+  },
+  cuerpoTarjeta: {
+    height: "80%",
+    borderBottomLeftRadius: 10,
+    borderBottomRightRadius: 10,
+    paddingHorizontal: "2%",
+  },
+  textoTarjeta: {
+    padding: "2%",
+    marginTop: "5%",
+    fontSize: 20,
+  },
+  contenedorOperacion: {
+    padding: "2%",
+    paddingTop: "5%",
+    flexDirection: "row",
+    justifyContent: "space-around",
+  },
+  botonOperacion: {
+    backgroundColor: colors.primary,
+    height: 40,
+    width: "40%",
+    alignItems: "center",
+    justifyContent: "center",
+    borderRadius: 10,
+  },
+  textoOperacion: {
+    color: colors.white,
+  },
+});
